ef0d5b53b4d3ee7727d42da563ff4df341b2b225
darrenwong916 <darrenwong06@gmail.com>
2014-02-19 16:28:40 -0800

    added delete route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ SpaApp::Application.routes.draw do
   get '/todos', to: 'todos#index', as: 'todos'
   post '/todos', to: 'todos#create'
   patch '/todos/:id', to: 'todos#update'
+  delete '/todos/:id', to: 'todos#destroy'
 end
